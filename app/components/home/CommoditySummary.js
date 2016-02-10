f6eac435bd00e54a1cf878f77725b263801a5409
nxy <nxy801111@hotmail.com>
2016-02-10 21:03:57 +0800

    主页

---
+++ app/components/home/CommoditySummary.js
@@ -0,0 +1,155 @@
+/**
+ *  Class: CommoditySummary
+ *  Author: Niu Xiaoyu
+ *  Date: 16/2/5.
+ *  Description: 商品简介
+ */
+import React from 'react-native';
+
+const { Component, View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, Alert } = React;
+const portrait = require('../../../assets/portrait.jpg');
+let THUMBS = [
+    require('../../../assets/sampleImage/img1.jpg'),
+    require('../../../assets/sampleImage/img2.jpg'),
+    require('../../../assets/sampleImage/img3.jpg'),
+    require('../../../assets/sampleImage/img4.jpg')
+];
+THUMBS = THUMBS.concat(THUMBS);
+
+const createThumbRow = (uri, i) => <Thumb key={i} uri={uri} />;
+
+const styles = StyleSheet.create({
+  page: {
+    height: 210,
+    backgroundColor: '#FFF',
+    marginBottom: 10
+  },
+  header: {
+    height: 50,
+    flexDirection: 'row'
+  },
+  portrait: {
+    height: 50,
+    width: 50
+  },
+  imagesContainer: {
+    flex: 185
+  },
+  horizontalScrollView: {
+    height: 120,
+  },
+  img: {
+    width: 100,
+    height: 100,
+  },
+  textSummary: {
+    flex: 100
+  },
+  op: {
+    flex: 50
+  },
+  font: {
+    paddingHorizontal: 5,
+    paddingVertical: 2,
+    fontSize: 12
+  },
+  price: {
+    fontSize: 20,
+    color: "red",
+    backgroundColor: '#f3f2f3'
+  }
+});
+
+export default class CommoditySummary extends Component {
+  // 默认属性
+  static defaultProps = {};
+
+  // 属性类型
+  static propTypes = {};
+
+  // 构造
+  constructor(props) {
+    super(props);
+    // 初始状态
+    this.state = {};
+  }
+
+  // 自定义方法
+  handle() {
+
+  }
+
+  // 渲染
+  render() {
+    const data = this.props.data;
+    return (
+      <View style={styles.page} onPress={this.props.actions.routes.commodityDetail()}>
+        <View style={styles.header}>
+          <Image style={styles.portrait} source={data.portrait} />
+          <View style={{width: 100, justifyContent: 'center'}}>
+            <Text style={styles.font}>{data.publisher}</Text>
+            <Text style={styles.font}>{data.publish_time}</Text>
+          </View>
+          <View style={{flex: 1, alignItems: 'flex-end', justifyContent: 'center'}}>
+            <Text style={[styles.font, styles.price]}>{`￥${data.price}`}</Text>
+          </View>
+        </View>
+        <View style={styles.imagesContainer}>
+          <ScrollView
+            directionalLockEnabled={true}
+            automaticallyAdjustContentInsets={false}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            directionalLockEnabled={true}
+            horizontal={true}
+            style={[styles.horizontalScrollView]}>
+            {data.thumbs.concat(data.thumbs).map(createThumbRow)}
+          </ScrollView>
+        </View>
+        <Text style={styles.font}>
+          {data.summary}
+        </Text>
+        <View style={{flexDirection: 'row', borderTopWidth: 1, borderColor: '#f3f2f3', paddingVertical: 3, paddingHorizontal: 10}}>
+          <View style={{flex: 1, alignItems: 'flex-start'}}>
+            <TouchableOpacity onPress={this.props.actions.routes.commodityDetail()}>
+              <Text style={styles.font}>详细信息</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
+            <TouchableOpacity onPress={this.leaveMessage.bind(this)}>
+              <Text style={styles.font}>留言</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.zan.bind(this)}>
+              <Text style={styles.font}>赞</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  leaveMessage() {
+    Alert.alert('提示', '为该商品的卖家留言?', [
+      {text: '确定'}
+    ])
+  }
+
+  zan() {
+    Alert.alert('提示', '为该商品的点赞?', [
+      {text: '确定'}
+    ])
+  }
+}
+
+class Thumb extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return false;
+  }
+  render() {
+    return (
+      <View style={{marginHorizontal: 2}} key={this.props.key}>
+        <Image style={styles.img} source={this.props.uri} />
+      </View>
+    );
+  }
+}
--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -8,11 +8,12 @@ import React from 'react-native';
 
 import Banner from '../../baseComponents/Banner';
 import HomeHeader from './Header';
+import CommoditySummary from './CommoditySummary';
 
-const { Component, View, Text, Image, StyleSheet, Platform } = React;
+const { Component, View, StyleSheet, Platform, ScrollView } = React;
 
 
-const mockData = [
+const mockData_banner = [
   {
     id: '1',
     url: require('../../../assets/banner/1.png')
@@ -31,8 +32,67 @@ const mockData = [
   }
 ];
 
+const mockData_commoditySummary = [
+  {
+    portrait: require('../../../assets/portrait.jpg'),
+    thumbs: [
+      require('../../../assets/sampleImage/img1.jpg'),
+      require('../../../assets/sampleImage/img2.jpg'),
+      require('../../../assets/sampleImage/img3.jpg'),
+      require('../../../assets/sampleImage/img4.jpg')
+    ],
+    publisher: '张三',
+    publish_time: '2016-1-2',
+    price: '410',
+    summary: '酷冷至尊（CoolerMaster） 烈焰枪 红轴 游戏机械键盘 台湾制造 优良血统 紧凑布局'
+  },
+  {
+    portrait: require('../../../assets/portrait.jpg'),
+    thumbs: [
+      require('../../../assets/sampleImage/img1.jpg'),
+      require('../../../assets/sampleImage/img2.jpg'),
+      require('../../../assets/sampleImage/img3.jpg'),
+      require('../../../assets/sampleImage/img4.jpg')
+    ],
+    publisher: '李四',
+    publish_time: '2015-11-2',
+    price: '900',
+    summary: '苏泊尔（SUPOR）32cm二代火红点不粘无油烟炒锅炒菜锅明火电磁炉通'
+  },
+  {
+    portrait: require('../../../assets/portrait.jpg'),
+    thumbs: [
+      require('../../../assets/sampleImage/img1.jpg'),
+      require('../../../assets/sampleImage/img2.jpg'),
+      require('../../../assets/sampleImage/img3.jpg'),
+      require('../../../assets/sampleImage/img4.jpg')
+    ],
+    publisher: '王五',
+    publish_time: '2016-1-5',
+    price: '50',
+    summary: '双立人锅具刀具12件套装 铸铁炖锅 煎锅蒸锅炊具 不锈钢色 锅具套装'
+  },
+  {
+    portrait: require('../../../assets/portrait.jpg'),
+    thumbs: [
+      require('../../../assets/sampleImage/img1.jpg'),
+      require('../../../assets/sampleImage/img2.jpg'),
+      require('../../../assets/sampleImage/img3.jpg'),
+      require('../../../assets/sampleImage/img4.jpg')
+    ],
+    publisher: '赵六',
+    publish_time: '2016-2-2',
+    price: '110',
+    summary: '中兴 Blade A1(C880A) 16G 灵动白 全网通'
+  }
+];
+
 const styles = StyleSheet.create(
   {
+    page: {
+      flex: 1,
+      backgroundColor: '#dbdee1'
+    },
     header: {
       backgroundColor: '#ffda44'
     },
@@ -66,14 +126,24 @@ export default class Home extends Component {
   // 渲染
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={styles.page}>
         <HomeHeader style={[styles.header, {height: Platform.OS === 'ios' ? 60 : 40}]} />
         <Banner
-          style={{height: 140, overflow: 'hidden'}}
-          source={mockData}
+          style={{height: 140, overflow: 'hidden', marginBottom: 10}}
+          source={mockData_banner}
           {...this.props}
         />
-        <Text>首页</Text>
+        <View style={{flex: 1, marginHorizontal: 3}}>
+          <ScrollView
+            directionalLockEnabled={true}
+            automaticallyAdjustContentInsets={false}
+          >
+            <CommoditySummary data={mockData_commoditySummary[0]} actions = {this.props.actions}/>
+            <CommoditySummary data={mockData_commoditySummary[1]} actions = {this.props.actions}/>
+            <CommoditySummary data={mockData_commoditySummary[2]} actions = {this.props.actions}/>
+            <CommoditySummary data={mockData_commoditySummary[3]} actions = {this.props.actions}/>
+          </ScrollView>
+        </View>
       </View>
     );
   }
